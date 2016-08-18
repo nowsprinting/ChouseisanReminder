dcd4be362281e23bf04975ea197ca6c54d7d487c
Koji Hasegawa <hasegawa@hubsys.co.jp>
2016-08-19 07:34:52 +0900

    Implement add/remove subscriber
    - LINEコールバックが友だち追加であれば、購読者としてMIDをデータストアに保存
    - LINEコールバックがブロックであれば、MIDをデータストアから削除
    - メッセージ一斉送信（データストアに保存されている全MIDに送信）を実装

--- main.go
+++ main.go
@@ -5,12 +5,30 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
+
+	"golang.org/x/net/context"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
 )
+
+// 購読者エンティティ
+type subscriber struct {
+	DisplayName string
+	MID         string
+}
+
+// 購読者の追加・削除ログを保存するエンティティ
+type logSubscriber struct {
+	DisplayName string
+	MID         string
+	OpType      linebot.OpType
+	AddTime     time.Time
+}
 
 func init() {
 	http.HandleFunc("/line/callback", lineCallback)
@@ -37,8 +55,7 @@ func lineCallback(w http.ResponseWriter, r *http.Request) {
 		log.Errorf(c, "Error occurred at parse `LINE_CHANNEL_ID`: %v", err)
 		return
 	}
-	bot, err := linebot.NewClient(channelID, channelSecret, channelMID,
-		linebot.WithHTTPClient(urlfetch.Client(c))) //Appengineのurlfetchを使用する
+	bot, err := linebot.NewClient(channelID, channelSecret, channelMID, linebot.WithHTTPClient(urlfetch.Client(c))) //Appengineのurlfetchを使用する
 	if err != nil {
 		log.Errorf(c, "Error occurred at create linebot client: %v", err)
 		return
@@ -57,20 +74,145 @@ func lineCallback(w http.ResponseWriter, r *http.Request) {
 	}
 	for _, result := range received.Results {
 		content := result.Content()
-		if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
-			text, err := content.TextContent()
+		if content != nil {
+			if content.IsOperation {
+				//オペレーションイベント受信
+				opContent, err := content.OperationContent()
+				if err != nil {
+					log.Errorf(c, "Error occurred at get operation content: %v", err)
+					return
+				}
+				sender, err2 := getSenderProfile(c, bot, opContent.Params[0])
+				if err2 != nil {
+					return
+				}
 
-			//送信者のディスプレイネームを取得
-			user, err := bot.GetUserProfile([]string{content.From})
+				if content.OpType == linebot.OpTypeAddedAsFriend {
+					if addFriend(c, sender.MID, sender.DisplayName) != nil {
+						return
+					}
+					sendToAll(c, bot, sender.DisplayName+"さんが購読を開始しました。いらっしゃいませ！！")
 
-			//メッセージ送信
-			_, err = bot.SendText([]string{content.From},
-				user.Contacts[0].DisplayName+"さんより\n「"+text.Text+"」")
-			if err != nil {
-				log.Errorf(c, "Error occurred at send text:%v", err)
+				} else if content.OpType == linebot.OpTypeBlocked {
+					if removeFriend(c, sender.MID, sender.DisplayName) != nil {
+						return
+					}
+					sendToAll(c, bot, sender.DisplayName+"さんが購読を解除しました。さようなら！")
+
+				} else {
+					log.Warningf(c, "Unknown OpType received. OpType=%v", content.OpType)
+				}
+
+			} else if content.IsMessage && content.ContentType == linebot.ContentTypeText {
+				//テキストメッセージ受信
+				text, err := content.TextContent()
+				if err != nil {
+					log.Errorf(c, "Error occurred at parse text context: %v", err)
+					return
+				}
+				sender, err2 := getSenderProfile(c, bot, content.From)
+				if err2 != nil {
+					return
+				}
+				sendToAll(c, bot, sender.DisplayName+"さんより\n「"+text.Text+"」")
 			}
 		}
 	}
+}
+
+/**
+ * 送信者の情報を取得する
+ */
+func getSenderProfile(c context.Context, bot *linebot.Client, from string) (linebot.ContactInfo, error) {
+	senderProfile, err := bot.GetUserProfile([]string{from})
+	if err != nil {
+		log.Errorf(c, "Error occurred at get sender profile. from: %v, err: %v", from, err)
+		return linebot.ContactInfo{}, err
+	}
+	return senderProfile.Contacts[0], nil //添字は0固定
+}
+
+/**
+ * 友だち追加（データストアに購読者として登録）
+ */
+func addFriend(c context.Context, mid string, displayName string) error {
+
+	//購読者を保存
+	entity := subscriber{
+		DisplayName: displayName,
+		MID:         mid,
+	}
+	key := datastore.NewKey(c, "Subscriber", mid, 0, nil)
+	if _, err := datastore.Put(c, key, &entity); err != nil {
+		log.Errorf(c, "Error occurred at put subcriber to datastore. mid:%v, err: %v", mid, err)
+		return err
+	}
+
+	//ログエントリを追加
+	logEntity := logSubscriber{
+		DisplayName: displayName,
+		MID:         mid,
+		OpType:      linebot.OpTypeAddedAsFriend,
+		AddTime:     time.Now(),
+	}
+	logKey := datastore.NewIncompleteKey(c, "LogSubscriber", nil)
+	if _, err := datastore.Put(c, logKey, &logEntity); err != nil {
+		log.Errorf(c, "Error occurred at put log-subcriber to datastore. mid:%v, err: %v", mid, err)
+		return err
+	}
+	return nil
+}
+
+/**
+ * 友だち削除（データストアから削除）
+ */
+func removeFriend(c context.Context, mid string, displayName string) error {
+
+	//購読者を削除
+	key := datastore.NewKey(c, "Subscriber", mid, 0, nil)
+	if err := datastore.Delete(c, key); err != nil {
+		log.Errorf(c, "Error occurred at delete subcriber to datastore. mid:%v, err: %v", mid, err)
+		return err
+	}
+
+	//ログエントリを追加
+	logEntity := logSubscriber{
+		DisplayName: displayName,
+		MID:         mid,
+		OpType:      linebot.OpTypeBlocked,
+		AddTime:     time.Now(),
+	}
+	logKey := datastore.NewIncompleteKey(c, "LogSubscriber", nil)
+	if _, err := datastore.Put(c, logKey, &logEntity); err != nil {
+		log.Errorf(c, "Error occurred at put log-subcriber to datastore. mid:%v, err: %v", mid, err)
+		return err
+	}
+	return nil
+}
+
+/**
+ * 購読者全員にメッセージを送信
+ */
+func sendToAll(c context.Context, bot *linebot.Client, message string) error {
+
+	//データストアから購読者のMIDを取得
+	q := datastore.NewQuery("Subscriber")
+	var subscribers []subscriber
+	if _, err := q.GetAll(c, &subscribers); err != nil {
+		log.Errorf(c, "Error occurred at get-all from datastore. err: %v", err)
+		return err
+	}
+	mids := make([]string, len(subscribers))
+	for i, current := range subscribers {
+		mids[i] = current.MID
+	}
+
+	//全員に送信
+	if _, err := bot.SendText(mids, message); err != nil {
+		log.Errorf(c, "Error occurred at send message: %v", err)
+		return err
+	}
+	return nil
 }
 
 /**
