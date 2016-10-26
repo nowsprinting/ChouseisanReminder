49051c251cc4897520dc4adc03cb3d7a0137d1f4
Koji Hasegawa <hasegawa@hubsys.co.jp>
2016-10-26 17:58:36 +0900

    Replace runtest.sh to Makefile (make test)

--- runtest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# define environment variables
-export CHOUSEISAN_EVENT_HASH=3f7ffd73ba174332ae05bd363eba8e71
-export LINE_CHANNEL_SECRET=012345678901234567890123456789ab
-export LINE_CHANNEL_ACCESS_TOKEN=u012345678901234567890123456789ab
-
-# run test
-goapp test -v -covermode=count -coverprofile=coverage.out
